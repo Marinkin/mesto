29de24f8f99eb13505debe0ef013f4c81343f353
Marina Dadayan <dadayaning@yandex.ru>
2020-07-09 20:18:31 +0300

    Проектная работа 4 (2)

--- scripts/script.js
+++ scripts/script.js
@@ -3,22 +3,30 @@ const closePopupButton = document.querySelector('.popup__closed-icon');
 const modal = document.querySelector('.popup');
 const form = modal.querySelector('.form');
 
-const InputName = document.querySelector ('.form__input_type_name');
-const InputJob = document.querySelector ('.form__input_type_job');
-const profileName = document.querySelector ('.profile__name');
-const profileProfession = document.querySelector ('.profile__profession');
+const inputName = document.querySelector('.form__input_type_name');
+const inputJob = document.querySelector('.form__input_type_job');
+const profileName = document.querySelector('.profile__name');
+const profileProfession = document.querySelector('.profile__profession');
 
 function togglePopup() {
     modal.classList.toggle('popup_opened');
 }
-openPopupButton.addEventListener('click', togglePopup);
+
+openPopupButton.addEventListener('click', function () {
+    togglePopup();
+    if (modal.classList.contains('popup_opened')) {
+        inputName.value = profileName.textContent;
+        inputJob.value = profileProfession.textContent;
+    }
+});
 closePopupButton.addEventListener('click', togglePopup);
 
-function formSubmitHandler (evt) {
+function formSubmitHandler(evt) {
     evt.preventDefault();
 
-    profileName.textContent = InputName.value;
-    profileProfession.textContent = InputJob.value;
+    profileName.textContent = inputName.value;
+    profileProfession.textContent = inputJob.value;
     togglePopup();
 }
+
 form.addEventListener('submit', formSubmitHandler);
